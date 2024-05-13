c169286e795c3fb0529dc0b2fecea205c7bbbb92
eamre <emrealkan.1@outlook.com>
2024-05-13 22:30:47 +0300

    Proje dosyası ekle.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse createdBrandResponse = new()
+            {
+                Name = request.Name,
+                Id = new Guid()
+            };
+
+            return createdBrandResponse;
+        }
+    }
+}
+
+
