59f02d2a3096c2df80ef36b0cc439ac515ecfcb5
eamre <emrealkan.1@outlook.com>
2024-05-14 00:54:53 +0300

    application layer implementation

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -13,6 +13,8 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
         return services;
     }
 }
--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +16,29 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
 
     public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
     {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
         public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
-            CreatedBrandResponse createdBrandResponse = new()
+            Brand brand = new()
             {
                 Name = request.Name,
-                Id = new Guid()
+                Id = Guid.NewGuid()
+            };
+
+            var result = await _brandRepository.AddAsync(brand);
+
+            CreatedBrandResponse createdBrandResponse = new()
+            {
+                Name = result.Name,
+                Id = result.Id
             };
 
             return createdBrandResponse;
